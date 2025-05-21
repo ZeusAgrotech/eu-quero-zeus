846cc61594e99bf1e736c55337eb1984d4150c94
Edson Simão Jr <edsonsjr@gmail.com>
2025-05-21 17:13:35 -0300

    feat: add Biome configuration and integrate BiomeJS for linting and formatting
    - Created a new `biome.json` file for BiomeJS configuration, enabling import organization and defining formatting rules.
    - Updated `package.json` to include `@biomejs/biome` as a development dependency.
    - Modified `package-lock.json` to reflect the addition of BiomeJS and its dependencies.
    - Added new assets: `bg-noise.png` and `logo-zeus-gradient.svg`.
    - Removed outdated SVG files: `file.svg`, `globe.svg`, `next.svg`, `vercel.svg`, and `window.svg`.

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,180 @@
-import Image from "next/image";
+'use client'
+
+import Image from 'next/image'
+import { useState } from 'react'
+
+type FormData = {
+  name: string
+  phone: string
+  area: number
+  crop: string
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const initialFormData: FormData = {
+    name: '',
+    phone: '',
+    area: 500,
+    crop: '',
+  }
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+  const [formData, setFormData] = useState<FormData>(initialFormData)
+  const [showOtherCrop, setShowOtherCrop] = useState(false)
+
+  const handleFormChange = (
+    event:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    const { name, value } = event.target
+    setShowOtherCrop(value === 'outra')
+    setFormData(prev => ({ ...prev, [name]: value }))
+  }
+
+  return (
+    <div className="isolate flex min-h-screen flex-col items-center justify-between p-6 lg:px-16">
+      <h1 className="font-bold text-4xl">Eu quero Zeus</h1>
+
+      <div className="flex w-full max-w-md flex-col items-center gap-4 rounded-4xl bg-gradient-to-br from-stone-50 to-stone-100 p-6 shadow-[0_3px_6px_rgba(0,0,0,0.3)]">
+        <Image
+          src="/assets/images/logo-zeus-gradient.svg"
+          alt="Logo do Zeus"
+          width={512}
+          height={150}
+          className="w-48 drop-shadow-[0_2px_2px_rgba(0,0,0,0.3)]"
+          role="presentation"
+        />
+
+        <form className="w-full space-y-4">
+          <div className="flex flex-col gap-2">
+            <label htmlFor="name" className="ml-1">
+              Como gostaria de ser chamado?
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="rounded-md border border-stone-300 bg-white px-4 py-2"
+              placeholder="Digite seu nome"
+              value={formData.name}
+              onChange={e => handleFormChange(e)}
+              required
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="phone" className="ml-1">
+              Celular/WhatsApp:
+            </label>
+            <input
+              type="tel"
+              id="phone"
+              name="phone"
+              placeholder="(__) _____-____"
+              className="rounded-md border border-stone-300 bg-white px-4 py-2"
+              value={formData.phone}
+              onChange={e => handleFormChange(e)}
+              required
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="area" className="ml-1">
+              Área de plantio: {formData.area} ha
+            </label>
+            <input
+              type="range"
+              id="area"
+              name="area"
+              className="w-full"
+              min={100}
+              step={100}
+              max={1000000}
+              value={formData.area}
+              onChange={e => handleFormChange(e)}
+              required
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="crop" className="ml-1">
+              Cultura plantada:
+            </label>
+            <select
+              id="crop"
+              name="crop"
+              className="rounded-md border border-stone-300 bg-white px-4 py-2"
+              required
+              value={formData.crop}
+              onChange={e => handleFormChange(e)}
+            >
+              <option value="">Selecione</option>
+              <option value="soja">Soja</option>
+              <option value="milho">Milho</option>
+              <option value="trigo">Trigo</option>
+              <option value="algodao">Algodão</option>
+              <option value="feijao">Feijão</option>
+              <option value="arroz">Arroz</option>
+              <option value="outra">Outra</option>
+            </select>
+            {showOtherCrop && (
+              <input
+                type="text"
+                id="otherCrop"
+                name="otherCrop"
+                placeholder="Qual?"
+                value={formData.crop}
+                onChange={e => handleFormChange(e)}
+                className="rounded-md border border-stone-300 bg-white px-4 py-2"
+              />
+            )}
+          </div>
+          <div className="flex flex-col gap-2">
+            <label
+              htmlFor="file"
+              className="ml-1 flex items-center justify-between"
+            >
+              <span className="flex items-center gap-2">
+                Anexar KML:{' '}
+                <span
+                  className="flex h-4 w-4 items-center justify-center rounded-full bg-stone-300 font-bold text-xs"
+                  title="Envie seu arquivo KML contendo suas áreas de plantio"
+                >
+                  ?
+                </span>
+              </span>
+              <span className="mr-1 text-[10px] text-stone-400 uppercase">
+                Opcional
+              </span>
+            </label>
+            <input
+              type="file"
+              id="file"
+              name="file"
+              className="rounded-md border border-stone-300 bg-white px-4 py-2"
+              accept=".kml, .kmz"
+              // value={formData.file}
+              // onChange={e => handleFormChange(e)}
+            />
+          </div>
+          <button type="submit" className="btn btn-primary mt-8 w-full">
+            Enviar
+          </button>
+        </form>
+      </div>
+
+      <p className="text-center text-black/70 text-sm">
+        &copy; ZEUSAGRO TECNOLOGIA E SERVIÇOS AGRÍCOLAS S/A,{' '}
+        {new Date().getFullYear()}.
+        <br />
+        CNPJ: 24.915.907/0001-49
+      </p>
+
+      <div
+        className="-z-[1] fixed inset-0 bg-[length:100_100] bg-[url(/assets/images/bg-noise.png)] mix-blend-overlay"
+        aria-hidden="true"
+      />
+      <div
+        className="-z-[2] absolute inset-0 bg-gradient-to-br from-zeus-300 to-zeus-500"
+        aria-hidden="true"
+      />
     </div>
-  );
+  )
 }
