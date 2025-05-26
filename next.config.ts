09b5fdb8046b580fb13d913c1b1b44196cf81e15
Edson Simão Jr <edsonsjr@gmail.com>
2025-05-26 16:21:20 -0300

    feat: update next.config.ts with basePath and trailingSlash settings

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev'],
   output: 'export',
+  basePath: '/euquerozeus',
+  trailingSlash: true,
 }
 
 export default nextConfig
